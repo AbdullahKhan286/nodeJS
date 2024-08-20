3a6ca51e073ede2e47c8dd8165cf1ff8d0e9b020
AbdullahKhan286 <20pwcse1916@uetpeshawar.edu.pk>
2024-08-20 20:27:09 +0500

    Model practice

---
+++ model/todo/todo.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      require: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodo',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
