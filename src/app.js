e6cb1f8a46812ed0afd34b9dc0786362faf72b39
AbdullahKhan286 <20pwcse1916@uetpeshawar.edu.pk>
2024-09-02 15:29:11 +0500

    Utils

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.use(cors({
     credentials: true,
 }));
 
+// extended: true for nested `req.body` objects
+app.use(express.urlencoded({extended: true,limit:'16kb'}));
+
+app.use(express.json({limit: '16kb'}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
 
 
 export {app};
