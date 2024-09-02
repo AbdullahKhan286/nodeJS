00c45442cbc4809d9d717f4462624ab60fb6fb6e
AbdullahKhan286 <20pwcse1916@uetpeshawar.edu.pk>
2024-09-02 13:00:30 +0500

    Updste

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/index.js';
 // configure .env file
-dotenv.config({path: './.env'});
+dotenv.config({ path: './.env' });
 
 const app = express();
 
-connectDB();
+connectDB().then(() => {
+    app.on('error', (err) => {
+        console.error('Express Error:', err);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+}).catch((error) => { console.log("This is DB Connection error", error); process.exit(1); });
 
 
 
